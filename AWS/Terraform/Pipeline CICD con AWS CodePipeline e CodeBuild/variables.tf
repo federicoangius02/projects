47e77114bc93b18e30cabd8848d7b4ef8d1848b1
federicoangius02 <federico.angius2@gmail.com>
2025-03-14 02:20:31 +0100

    Aggiornato main.tf e buildspec.yaml

--- AWS/Terraform/Pipeline CICD con AWS CodePipeline e CodeBuild/variables.tf
+++ AWS/Terraform/Pipeline CICD con AWS CodePipeline e CodeBuild/variables.tf
@@ -1,5 +1,5 @@
 variable "github_token" {
   description = "Token OAuth per accedere al repository GitHub"
   type        = string
-  sensitive   = true  # Imposta come sensibile per nascondere il valore negli output
+  sensitive   = true # Imposta come sensibile per nascondere il valore negli output
 }
